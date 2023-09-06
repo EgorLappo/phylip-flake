e4ad19a5b17ceceef7bcc521a95ebdf7228fde92
Egor Lappo <elappo@stanford.edu>
2023-09-05 18:27:53 -0700

    flake complete and works

--- flake.nix
+++ flake.nix
@@ -1,11 +1,51 @@
 {
-  description = "A very basic flake";
+  description = "PHYLIP";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
   };
+
+  outputs = { self, nixpkgs, flake-utils }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        phylip-drv = pkgs.stdenv.mkDerivation
+          {
+            name = "phylip";
+            version = "3.698";
+            src = pkgs.fetchzip {
+              url = "http://evolution.gs.washington.edu/phylip/download/phylip-3.698.zip";
+              hash = "sha256-00MnGDhK+n0Eut2brL9w1gfZ1fdXUpmVPAZHlVew6lA=";
+            };
+
+            # a fix for some problem in the code connected with double importing the same .h file and absence of "extern" keyword
+            NIX_CFLAGS_COMPILE = "-O3 -DUNX -fomit-frame-pointer -fcommon";
+
+            buildInputs = [
+              pkgs.gcc
+            ];
+
+            buildPhase = ''
+              cd src
+              make -f Makefile.unx install
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp -r ../exe/* $out/bin
+            '';
+
+          };
+      in
+      rec
+      {
+
+        packages.default = phylip-drv;
+
+      });
 }
